89dcff921b0384643038f1eae97fdb18a60d1d19
ksummerill <kaitlynsummerill@gmail.com>
2020-10-07 16:59:24 -0700

    build form handler and rudimentary form in html

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const endPoint = "http://localhost:3000/api/v1/businesses"
 
 document.addEventListener('DOMContentLoaded', () => {
   getBusinesses()
+
+  const createBusinessForm = document.querySelector("#create-syllabus-form")
+
+  createBusinessForm.addEventListener("submit", (e) => createFormHandler(e))
 })
   // initial fetch to grab all businesses from index controller
   function getBusinesses() {
@@ -21,4 +25,17 @@ document.addEventListener('DOMContentLoaded', () => {
       });
 
     })
+  }
+
+  function createFormHandler(e) {
+    e.preventDefault()
+    const nameInput = document.querySelector('#input-name').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const websiteInput = document.querySelector('#input-website').value
+    const streetAddressInput = document.querySelector('#input-street_address').value
+    const cityInput = document.querySelector('#input-city').value
+    const stateInput = document.querySelector('#input-state').value
+    const zipcodeInput = document.querySelector('#input-zipcode').value
+    const categoryId = parseInt(document.querySelector('#categories').value)
+    postFetch(nameInput, descriptionInput, websiteInput, streetAddressInput, cityInput, stateInput, zipcodeInput, categoryId)
   }
